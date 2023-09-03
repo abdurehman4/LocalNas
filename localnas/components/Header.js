b011502e2c007ecbde9bb20f5cbf05da6ea350df
Abdur Rehman Imran <arehmanimran4@gmail.com>
2023-09-03 18:14:10 +0500

    Header fix

--- localnas/components/Header.js
+++ localnas/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
   const [navBar, setNavBar] = useState(false)
   return (
     // <ClientComponent>
-    <header className='sticky flex flex-row md:flex-row top-0 z-10 bg-white outline outline-1 outline-slate-200'>
+    <header className='sticky flex flex-row justify-right md:flex-row top-0 z-10 bg-white outline outline-1 outline-slate-200'>
       <Link href='/'>
         <div className='flex flex-row p-0 items-center justify-items-center justify-self-center'>
           <Image alt='' className='h-20 w-20' src={icon} />
@@ -22,10 +22,10 @@ const Header = () => {
         <li className='hover-underline-animation text-zinc-600 font-medium'><Link href="/media" className="text-center">Media</Link></li>
         <li className='hover-underline-animation text-zinc-600 font-medium'><Link href="/compressed" className="text-center">Compressed</Link></li>
       </ul>
-      <Link href=''><Image className="md:hidden h-8 w-7" alt='' src={bar} key="hek" onClick={() => setNavBar(!navBar)} /></Link>
+      <Link className='md:hidden' href=''><Image className="md:hidden h-8 w-7" alt='' src={bar} key="hek" onClick={() => setNavBar(!navBar)} /></Link>
       {
         navBar && (
-          <div className="absolute ease-in-out duration-300 right-0 z-10 mt-50 top-16 w-56 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+          <div className="absolute md:hidden ease-in-out duration-300 right-0 z-10 mt-50 top-16 w-56 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
             <div className="py-1" role="none">
               <Link href="/" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Home</Link>
               <Link href="/documents" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Documents</Link>
